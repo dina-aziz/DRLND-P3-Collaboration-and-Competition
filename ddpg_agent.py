baf6d148076989559de3e3803ce909015a5650ad
Dina Aziz <dinamagdhelmy@gmail.com>
2019-11-21 16:30:00 -0400

    Achieving desired score

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -12,9 +12,9 @@ import torch.optim as optim
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 256        # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 2e-3              # for soft update of target parameters
-LR_ACTOR = 2e-4         # learning rate of the actor 
-LR_CRITIC = 2e-3        # learning rate of the critic
+TAU = 3e-1              # for soft update of target parameters
+LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_CRITIC = 1e-4        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -107,7 +107,6 @@ class Agent():
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
-        torch.nn.utils.clip_grad_norm(self.critic_local.parameters(),1)
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
